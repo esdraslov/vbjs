281d2a3f553f764e9d86cf9489f5ba615dd9c25d
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-09-05 22:51:04 -0300

    Update vbjs.js

--- public/vbjs.js
+++ public/vbjs.js
@@ -1,4 +1,4 @@
 function MsgBox(message, title="VBscript by js") {
-  let window = window.open(`https://esdraslov.github.io/vbjs/popup?msg=${message}&title=${title}`) // i dont know how i make the arg detect ;-;
+  let window = window.open(`https://msgbox-vbjs.esdraslov.repl.co/?msg=${message}&title=${title}`)
   return window
 }
